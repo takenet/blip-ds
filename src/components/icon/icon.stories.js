5cf817570b4614e4b29affd8e156926fae88847e
Jonathan Cruz Rocha Ferreira <jonathanc@take.net>
2019-12-26 14:26:17 -0300

    feat(configs): added webpack storybook
    ISSUES CLOSED: #1
    
    fea/storybook

--- src/components/icon/icon.stories.js
+++ src/components/icon/icon.stories.js
@@ -5,9 +5,13 @@ export default {
 export const iconDefault = () => '<bds-icon name="user"></bds-icon>';
 
 export const iconSizes = () => `
+  <bds-icon name="user" size="xxx-large"></bds-icon>
+  <bds-icon name="user" size="xx-large"></bds-icon>
+  <bds-icon name="user" size="x-large"></bds-icon>
   <bds-icon name="user" size="large"></bds-icon>
   <bds-icon name="user" size="medium"></bds-icon>
   <bds-icon name="user" size="small"></bds-icon>
   <bds-icon name="user" size="x-small"></bds-icon>
   <bds-icon name="user" size="xx-small"></bds-icon>
+  <bds-icon name="user" size="xxx-small"></bds-icon>
 `;
