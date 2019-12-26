ed494346bf768df0e0b73219c794095e05a7858d
Jonathan Cruz Rocha Ferreira <jonathanc@take.net>
2019-12-26 14:06:38 -0300

    feat(configs): added example using storybook
    ISSUES CLOSED: #1
    
    fea/storybook

---
+++ src/components/icon/icon.stories.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Icon',
+};
+
+export const iconDefault = () => '<bds-icon name="user"></bds-icon>';
+
+export const iconSizes = () => `
+  <bds-icon name="user" size="large"></bds-icon>
+  <bds-icon name="user" size="medium"></bds-icon>
+  <bds-icon name="user" size="small"></bds-icon>
+  <bds-icon name="user" size="x-small"></bds-icon>
+  <bds-icon name="user" size="xx-small"></bds-icon>
+`;
