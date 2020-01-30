af9638f5f2c4167a3c35092a9646bfd77ceae6f2
Jonathan Cruz Rocha <jonathanc@take.net>
2020-01-30 17:28:59 -0300

    feat(component): add radio
    feature/radio

---
+++ src/components/radio/radio.tsx
@@ -0,0 +1,15 @@
+import { Component, h } from "@stencil/core";
+
+@Component({
+    tag: 'bds-radio',
+    styleUrl: 'radio.scss',
+    shadow: true
+})
+export class Radio {
+
+    render(): HTMLElement {
+        return (
+            <div> Radio </div>
+        )
+    }
+}
