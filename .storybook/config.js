af0a803e7b82ba0e4c7f8c58598e7462ad85f649
Jonathan Cruz Rocha Ferreira <jonathanc@take.net>
2020-02-29 20:29:41 -0300

    feat(component): icon refactor
    feature/alter-icon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,21 +1,11 @@
-import { configure, addParameters, addDecorator } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-import { withTests } from "@storybook/addon-jest";
-
-import results from "../jest-test-results.json";
 
 addParameters({
-    viewport: {
-        viewports: INITIAL_VIEWPORTS,
-    },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
 });
-
-addDecorator(
-    withTests({
-        results,
-        filesExt: ".spec.ts"
-    })
-);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components', true, /\.stories\.jsx$/), module);
