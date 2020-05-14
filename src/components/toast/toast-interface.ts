8218acf51b96c868a330635ae5e546f98f4b3ba5
Vinicius Oliveira Souza <viniciusos@take.net>
2020-05-14 15:52:33 -0300

    feat: removing unused type
    feature/toast_default_icons

--- src/components/toast/toast-interface.ts
+++ src/components/toast/toast-interface.ts
@@ -6,17 +6,6 @@ export type ButtonActionType = 'close' | 'custom';
 
 export type IconVariantMap = { [key in VariantType]: string };
 
-export type OpenToastType = {
-  buttonAction: ButtonActionType;
-  buttonText: string;
-  toastText: string;
-  toastTitle: string;
-  icon?: string;
-  actionType?: ActionType;
-  variant?: VariantType;
-  duration?: number;
-};
-
 export type CreateToastType = {
   buttonAction: ButtonActionType;
   buttonText: string;
