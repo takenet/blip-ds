270a69b6046cc00bbebd7254e11e9ce908358ed3
Jonathan Cruz Rocha <joohncruzrocha@gmail.com>
2020-05-27 11:27:05 -0300

    feat(component): added chips and remove chip on chips
    feature/chips_component
    
    HEAD

--- src/components/chip/chip.spec.tsx
+++ src/components/chip/chip.spec.tsx
@@ -4,13 +4,6 @@ import { Chip } from './chip';
 describe('bds-chip', () => {
   let page;
 
-  const getWithText = async () => {
-    return await newSpecPage({
-      components: [Chip],
-      html: `<bds-chip>Order by asc</bds-chip>`,
-    });
-  };
-
   beforeEach(async () => {
     page = await newSpecPage({
       components: [Chip],
