2c0fad9c41f83b0507ed14a4cdbd636320d1c131
Paula Cristina Oliveira <paula.oliveira@take.net>
2021-02-26 09:47:03 -0300

    feat(switch): test part II

--- src/components/bds-switch/test/bds-switch.spec.tsx
+++ src/components/bds-switch/test/bds-switch.spec.tsx
@@ -3,18 +3,18 @@ import { Switch } from '../bds-switch';
 
 describe('bds-switch', () => {
   const getPage = async (
-    { size, disabled, checked } = { size: null, disabled: null, checked: null }
+    { size, disabled, checked } = { size: '', disabled: null, checked: null }
   ): Promise<SpecPage> => {
     const getSwitchElement = (): string => {
-      if (size != null && disabled && checked) {
+      if (size != null && size != '' && disabled && checked) {
         return `<bds-switch size=${size} disabled=${disabled} checked=${checked}></bds-switch>`;
       }
 
-      if (size != null && disabled) {
+      if (size != null && size != '' && disabled) {
         return `<bds-switch size=${size} disabled=${disabled} ></bds-switch>`;
       }
 
-      if (size != null && checked) {
+      if (size != null && size != '' && checked) {
         return `<bds-switch size=${size}  checked=${checked}></bds-switch>`;
       }
 
@@ -22,7 +22,7 @@ describe('bds-switch', () => {
         return `<bds-switch  disabled=${disabled} checked=${checked}></bds-switch>`;
       }
 
-      if (size != null) {
+      if (size != null && size != '') {
         return `<bds-switch size=${size}></bds-switch>`;
       }
 
@@ -68,17 +68,40 @@ describe('bds-switch', () => {
     expect(page.root.shadowRoot.querySelector('disabled')).toBeFalsy();
   });
 
-  it('should render the disabled passed by prop', async () => {
+  it('should render the disabled=true passed by prop', async () => {
     const page = await getPage({ size: 'short', disabled: true, checked: null });
     expect(page.root.shadowRoot.querySelector('.switch')).toBeTruthy();
     expect(page.root.shadowRoot.querySelector('.switch--size-short')).toBeTruthy();
     expect(page.root.shadowRoot.querySelector('.slider--deselected-disabled')).toBeTruthy();
   });
 
-  it('should render the checked passed by prop', async () => {
-    const page = await getPage({ size: 'short', disabled: true, checked: true });
+  it('should render the checked=true passed by prop', async () => {
+    const page = await getPage({ size: null, disabled: true, checked: true });
     expect(page.root.shadowRoot.querySelector('.switch')).toBeTruthy();
-    expect(page.root.shadowRoot.querySelector('.switch--size-short')).toBeTruthy();
+    expect(page.root.shadowRoot.querySelector('.switch--size-standard')).toBeTruthy();
     expect(page.root.shadowRoot.querySelector('.slider--selected-disabled')).toBeTruthy();
+  });
+
+  it('should render the checked=false passed by prop', async () => {
+    const page = await getPage({ size: null, disabled: false, checked: false });
+    expect(page.root.shadowRoot.querySelector('.switch')).toBeTruthy();
+    expect(page.root.shadowRoot.querySelector('.switch--size-standard')).toBeTruthy();
+    expect(page.root.shadowRoot.querySelector('.slider--deselected')).toBeTruthy();
+  });
+
+  it('should execute a custom action on switch click', async () => {
+    const page = await getPage({ size: null, disabled: false, checked: false });
+    const bdsswitch = page.body.querySelector('bds-switch');
+    const _callback = jest.fn();
+    page.doc.addEventListener('onBdsChange', _callback);
+
+    await page.waitForChanges();
+
+    bdsswitch.shadowRoot.querySelector('.switch');
+    bdsswitch.onchange;
+
+    await page.waitForChanges();
+
+    expect(_callback).toHaveBeenCalled();
   });
 });
