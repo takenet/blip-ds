406148fc8a5b910de83bd28802c4530084c96e1e
Brian Bruno Emanuel Brandão Silva <brian.silva@take.net>
2021-03-01 15:27:11 -0300

    fix(modal): fixing component height and width.
    feature/add-modal-component

--- src/components/modal/modal.stories.jsx
+++ src/components/modal/modal.stories.jsx
@@ -14,7 +14,7 @@ export default {
 export const defaultModal = () => (
   <>
     <bds-modal open={boolean('open', true)} close-button={boolean('close-button', false)}>
-      <div style={{ display: 'flex', alignItems: 'center', height: '343px' }}>
+      <div style={{ display: 'flex', alignItems: 'center', height: '288px' }}>
         <div style={{paddingRight: '20px'}}>
           <img src="https://picsum.photos/200/210?random=2"/>
         </div>
