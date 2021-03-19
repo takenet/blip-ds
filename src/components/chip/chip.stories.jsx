ba64b97be228512815f829da786f6491093758d4
Willian Lomeu Machado <willian.lomeu@take.net>
2021-03-19 14:34:00 -0300

    fix(chips): add more chips examples

--- src/components/chip/chip.stories.jsx
+++ src/components/chip/chip.stories.jsx
@@ -21,13 +21,50 @@ const spaceBetween = {
   margin: '8px'
 }
 
-export const chips = () => {
+export const chipsClikable = () => {
   return(
      <div  style={defaultButtonStyle}>
-      <bds-chip style={spaceBetween} variant="default" >chips default</bds-chip>
-      <bds-chip style={spaceBetween} variant="primary" >chips primary</bds-chip>
-      <bds-chip style={spaceBetween} danger="true" >chips delete</bds-chip>
-      <bds-chip style={spaceBetween} filter="true" >chips filter</bds-chip>
+      <bds-chip style={spaceBetween} clikable="true" variant="primary" >chips primary</bds-chip>
+    </div>
+  )
+};
+
+export const chipsDanger = () => {
+  return(
+     <div  style={defaultButtonStyle}>
+      <bds-chip style={spaceBetween} danger="true" >chips primary</bds-chip>
+    </div>
+  )
+};
+
+export const chipsDeletable = () => {
+  return(
+     <div  style={defaultButtonStyle}>
+      <bds-chip style={spaceBetween} deletable="true" variant="primary" >chips primary</bds-chip>
+    </div>
+  )
+};
+
+export const chipsDisabled = () => {
+  return(
+     <div  style={defaultButtonStyle}>
+      <bds-chip style={spaceBetween} disabled="true" variant="primary" >chips primary</bds-chip>
+    </div>
+  )
+};
+
+export const chipsFilter = () => {
+  return(
+     <div  style={defaultButtonStyle}>
+      <bds-chip style={spaceBetween} filter="true" >chips primary</bds-chip>
+    </div>
+  )
+};
+
+export const chipsIcon = () => {
+  return(
+     <div  style={defaultButtonStyle}>
+      <bds-chip style={spaceBetween} icon="warning" variant="primary" >chips primary</bds-chip>
     </div>
   )
 };
@@ -37,6 +74,17 @@ export const chipsSize = () => {
      <div  style={defaultButtonStyle}>
       <bds-chip style={spaceBetween} variant="primary" size="standard" >chips default</bds-chip>
       <bds-chip style={spaceBetween} variant="primary" size="tall" >chips primary</bds-chip>
+    </div>
+  )
+};
+
+export const chipsVariant = () => {
+  return(
+     <div  style={defaultButtonStyle}>
+      <bds-chip style={spaceBetween} variant="default" >chips default</bds-chip>
+      <bds-chip style={spaceBetween} variant="primary" >chips primary</bds-chip>
+      <bds-chip style={spaceBetween} danger="true" >chips delete</bds-chip>
+      <bds-chip style={spaceBetween} filter="true" >chips filter</bds-chip>
     </div>
   )
 };
