c36d8dd6d2eaa01fae7f83a1b748aafbf81534eb
Joao Pedro Lira Dos Santos <jpls@cin.ufpe.br>
2021-04-15 18:37:00 -0300

    feat(select-chips): create a method to get the internal chips

--- src/components.d.ts
+++ src/components.d.ts
@@ -730,6 +730,10 @@ export namespace Components {
          */
         "errorMessage"?: string;
         /**
+          * Return the internal chips.
+         */
+        "getChips": () => Promise<string[]>;
+        /**
           * used for add icon in input left. Uses the bds-icon component.
          */
         "icon"?: string;
