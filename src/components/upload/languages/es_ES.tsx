831f6d6fa7719ef882815446b2b61c20aff5114b
Willian Lomeu Machado <willian.lomeu@take.net>
2022-06-13 18:31:27 -0300

    feat(upload): upgrade the upload logic

--- src/components/upload/languages/es_ES.tsx
+++ src/components/upload/languages/es_ES.tsx
@@ -1,6 +1,6 @@
 export const esTerms = [
   {
-    uploaded: 'Archivo subido',
+    uploaded: 'Archivos subidos',
     dropHere: 'Soltar aquí para adjuntar archivo',
     dropOrClick: 'Arrastre y suelte sus archivos aquí o haga clic para cargar el archivo',
   },
