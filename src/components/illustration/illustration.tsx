78a4b865780935e28cc906e770b01be4bdb5a7ea
Lucas Murta <lucas.murta@take.net>
2023-12-22 11:25:10 -0300

    fix(illustration): change illustration json

--- src/components/illustration/illustration.tsx
+++ src/components/illustration/illustration.tsx
@@ -29,51 +29,13 @@ export class BdsIllustration {
     this.setIllustrationContent();
   }
 
-  /**Function to get the name e complete with the path in tokens*/
-  getIllustration = () => {
-    const name = this.name;
-    const type = this.type;
-    if (type === 'default') {
-      const fullName = `asset-illustration-${name}`;
-      return fullName;
-    } else if (type === 'screens') {
-      const fullName = `asset-illustration-screens-${name}`;
-      return fullName;
-    } else if (type === 'blip-outline') {
-      const fullName = `asset-illustration-blip-outline-${name}`;
-      return fullName;
-    } else if (type === 'blip-solid') {
-      const fullName = `asset-illustration-blip-solid-${name}`;
-      return fullName;
-    } else if (type === 'logo-integration') {
-      const fullName = `asset-illustration-logo-integration-${name}`;
-      return fullName;
-    } else if (type === 'brand') {
-      const fullName = `asset-illustration-brand-${name}`;
-      return fullName;
-    } else if (type === 'empty-states') {
-      const fullName = `asset-illustration-empty-states-${name}`;
-      return fullName;
-    } else if (type === 'segmented') {
-      const fullName = `asset-illustration-segmented-${name}`;
-      return fullName;
-    } else if (type === 'smartphone') {
-      const fullName = `asset-illustration-smartphone-${name}`;
-      return fullName;
-    } else if (type === 'spots') {
-      const fullName = `asset-illustration-spots-${name}`;
-      return fullName;
-    }
-  };
-
   /**Function to map the svg and call the "formatSvg" function */
   setIllustrationContent = () => {
-    const key = this.getIllustration();
     const tokensVersion = packageJson.dependencies['blip-tokens'].replace('^', '');
-    const apiUrl = `https://cdn.jsdelivr.net/npm/blip-tokens@${tokensVersion}/build/json/assets_illustrations.json`;
+    const apiUrl = `https://cdn.jsdelivr.net/npm/blip-tokens@${tokensVersion}/build/json/illustrations/${this.type}/${this.name}.json`;
     fetch(apiUrl).then((response) =>
       response.json().then((data) => {
-        this.IllustrationContent = data[key];
+        this.IllustrationContent = data[`asset-${this.type}-${this.name}-svg`];
       })
     );
   };
