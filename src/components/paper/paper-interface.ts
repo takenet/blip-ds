667e44da5b619f9ce1003ef70855d7aa9d412fbc
WillianLomeu <willianlomeu.contato@gmail.com>
2024-03-22 11:01:29 -0300

    fix(paper): added background color possibility

--- src/components/paper/paper-interface.ts
+++ src/components/paper/paper-interface.ts
@@ -3,3 +3,5 @@ export type PaperElevation = 'static' | 'primary' | 'secondary' | 'none';
 export type PaperElevationMap = {
   [key in PaperElevation]: string;
 };
+
+export type PaperBackground = 'surface-1' | 'surface-2' | 'surface-3' | 'surface-4';
