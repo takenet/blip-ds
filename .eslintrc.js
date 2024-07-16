6cae34963ca0910e8ae339431f4d3f77acae4a26
WillianLomeu <willianlomeu.contato@gmail.com>
2024-07-15 21:23:10 -0300

    feat(prettier): fixing build error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
