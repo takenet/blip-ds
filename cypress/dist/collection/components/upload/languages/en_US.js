e76cfa5523d92a3b0b644b21020a3bb4843911c8
Lucas Murta <lucas.murta@take.net>
2024-10-10 10:42:53 -0300

    feat(cypress): remove cypress files build

--- cypress/dist/collection/components/upload/languages/en_US.js
+++
@@ -1,7 +0,0 @@
-export const enTerms = [
-  {
-    uploaded: 'Files uploaded',
-    dropHere: 'Drop here to attach file',
-    dropOrClick: 'Drag and drop your files here or click to upload file',
-  },
-];
