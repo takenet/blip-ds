647ad8ad9004ed1446b7b496c7890e066a733626
WillianLomeu <willianlomeu.contato@gmail.com>
2025-03-19 18:53:07 -0300

    feat(upload): new component ui

--- src/components/upload/languages/pt_BR.tsx
+++ src/components/upload/languages/pt_BR.tsx
@@ -2,7 +2,8 @@ export const ptTerms = [
   {
     uploaded: 'Arquivos enviados',
     dropHere: 'Solte aqui para anexar o arquivo',
-    dropOrClick: 'Arraste e solte seus arquivos aqui ou clique para fazer upload do arquivo',
+    dropOrClick: 'ou solte os arquivos aqui',
     formatError: 'Ocorreu um erro ao anexar o arquivo, tente novamente ou selecione outro arquivo',
+    chooseFiles: 'Escolher arquivos',
   },
 ];
