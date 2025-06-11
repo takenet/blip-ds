ffdef59b5d2a89644dd83e85b735632614ed7cdb
Lucas Soares <lucas.soares@blip.ai>
2025-06-11 10:18:27 -0300

    ci: change pipeline job names

--- .cz-config.js
+++ .cz-config.js
@@ -1,5 +1,6 @@
 module.exports = {
     types: [
+      { value: 'ci', name: 'ci:     Change in the CI' },
       { value: 'feat', name: 'feat:     A new feature' },
       { value: 'fix', name: 'fix:      A bug fix' },
       { value: 'docs', name: 'docs:     Documentation only changes' },
