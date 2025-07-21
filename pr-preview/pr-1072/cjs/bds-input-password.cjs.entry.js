17a54f7c36ab83632d03ce97baec211d9dff3c08
victorguimaraes2153 <victorguimaraes2153@users.noreply.github.com>
2025-07-21 15:05:29 +0000

    Deploy preview for PR 1072 🛫

---
+++ pr-preview/pr-1072/cjs/bds-input-password.cjs.entry.js
@@ -0,0 +1,159 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const index = require('./index-6a53aecf.js');
+
+const inputPasswordCss = ":host{display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;position:relative;width:100%;max-width:100%;max-height:100%}:host input{-webkit-box-shadow:inherit;box-shadow:inherit}:host input::-webkit-input-placeholder{color:var(--color-content-ghost, #8c8c8c);opacity:1}:host input::-moz-placeholder{color:var(--color-content-ghost, #8c8c8c);opacity:1}:host input:-ms-input-placeholder{color:var(--color-content-ghost, #8c8c8c);opacity:1}:host input::-ms-input-placeholder{color:var(--color-content-ghost, #8c8c8c);opacity:1}:host input::placeholder{color:var(--color-content-ghost, #8c8c8c);opacity:1}:host input::-webkit-input-placeholder{color:var(--color-content-ghost, #8c8c8c);opacity:1}.input{display:-ms-flexbox;display:flex;-ms-flex-direction:row;flex-direction:row;-ms-flex-align:center;align-items:center;padding:8px 4px 9px 12px;-ms-flex:1;flex:1;width:100%;max-width:100%;max-height:100%}.input .bds-icon{position:relative;z-index:1}.input--state-primary{border:1px solid var(--color-border-1, rgba(0, 0, 0, 0.2));-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px}.input--state-primary .input__icon{position:relative}.input--state-primary .input__icon::before{content:\"\";position:absolute;top:0;left:0;width:100%;height:100%;background-color:var(--color-hover, rgba(0, 0, 0, 0.08));z-index:0;opacity:50%;border-radius:8px}.input--state-primary:hover{border:1px solid var(--color-primary, #1e6bf1);-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px}.input--state-primary.input--pressed{border:1px solid var(--color-primary, #1e6bf1);-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px;-webkit-box-shadow:0 0 0 2px var(--color-info, #80e3eb);box-shadow:0 0 0 2px var(--color-info, #80e3eb)}.input--state-primary.input--pressed .input__icon .bds-icon{color:var(--color-primary, #1e6bf1)}.input--state-primary .input__container__label{color:var(--color-content-default, #282828)}.input--state-primary .input__container__label--pressed bds-typo{color:var(--color-primary, #1e6bf1)}.input--state-primary .input__container__text{caret-color:var(--color-primary, #1e6bf1);color:var(--color-content-default, #282828)}.input--state-danger{border:1px solid var(--color-delete, #e60f0f);-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px}.input--state-danger .input__icon{position:relative}.input--state-danger .input__icon::before{content:\"\";position:absolute;top:0;left:0;width:100%;height:100%;background-color:var(--color-surface-negative, #8a0000);z-index:0;opacity:50%;border-radius:8px}.input--state-danger:hover{border:1px solid var(--color-negative, #e60f0f);-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px}.input--state-danger.input--pressed{border:1px solid var(--color-negative, #e60f0f);-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px;-webkit-box-shadow:0 0 0 2px var(--color-error, #fabebe);box-shadow:0 0 0 2px var(--color-error, #fabebe)}.input--state-danger.input--pressed .input__icon .bds-icon{color:var(--color-negative, #e60f0f)}.input--state-danger .input__container__label{color:var(--color-delete, #e60f0f)}.input--state-danger .input__container__label--pressed bds-typo{color:var(--color-negative, #e60f0f)}.input--state-danger .input__container__text{caret-color:var(--color-negative, #e60f0f);color:var(--color-content-default, #282828)}.input--state-success{border:1px solid var(--color-border-1, rgba(0, 0, 0, 0.2));-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px}.input--state-success .input__icon{position:relative}.input--state-success .input__icon::before{content:\"\";position:absolute;top:0;left:0;width:100%;height:100%;background-color:var(--color-surface-positive, #01723e);z-index:0;border-radius:8px}.input--state-success:hover{border:1px solid var(--color-positive, #10603b);-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px}.input--state-success.input--pressed{border:1px solid var(--color-positive, #10603b);-webkit-box-sizing:border-box;box-sizing:border-box;border-radius:8px;-webkit-box-shadow:0 0 0 2px var(--color-success, #84ebbc);box-shadow:0 0 0 2px var(--color-success, #84ebbc)}.input--state-success.input--pressed .input__icon .bds-icon{color:var(--color-positive, #10603b)}.input--state-success .input__container__label{color:var(--color-content-default, #282828)}.input--state-success .input__container__label--pressed bds-typo{color:var(--color-positive, #10603b)}.input--state-success .input__container__text{caret-color:var(--color-positive, #10603b);color:var(--color-content-default, #282828)}.input--state-disabled{opacity:50%;pointer-events:none;cursor:not-allowed}.input--state-disabled .input__icon{position:relative}.input--state-disabled .input__icon::before{content:\"\";position:absolute;top:0;left:0;width:100%;height:100%;background-color:var(--color-hover, rgba(0, 0, 0, 0.08));z-index:0;opacity:50%;border-radius:8px}.input .icon-success{color:var(--color-positive, #10603b);margin-left:4px}.input--label{padding:7px 4px 8px 12px}.input__icon{cursor:inherit;display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;border-radius:8px;margin-right:8px;padding:2.5px}.input__icon--large{padding:4px}.input__container{display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;-ms-flex-pack:center;justify-content:center;width:100%}.input__container__wrapper{display:-ms-flexbox;display:flex;-ms-flex-wrap:wrap;flex-wrap:wrap}.input__container__wrapper__chips{display:inline;max-height:100px;overflow:auto}.input__container__wrapper__chips::-webkit-scrollbar{width:16px;background-color:var(--color-shadow-0, rgba(0, 0, 0, 0.04));border-radius:10px}.input__container__wrapper__chips::-webkit-scrollbar-thumb{border-radius:10px;border:4px solid transparent;border-radius:10px;background-clip:content-box;background-color:var(--color-border-1, rgba(0, 0, 0, 0.2))}.input__container__label{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center}.input__container__text{display:inline-block;margin:0;border:0;padding:0;width:auto;vertical-align:middle;white-space:normal;line-height:inherit;background:none;color:inherit;font-size:inherit;font-family:inherit;-webkit-box-sizing:content-box;-moz-box-sizing:content-box;box-sizing:content-box;font-family:\"Nunito Sans\", \"Carbona\", \"Tahoma\", \"Helvetica\", \"Arial\", sans-serif;font-size:0.875rem;line-height:150%;resize:none;cursor:inherit}.input__container__text:focus{outline:0}.input__container__text::-webkit-file-upload-button{padding:0;border:0;background:none}.input__container__text:focus{outline:0}.input__container__text[type=checkbox],.input__container__text[type=radio]{width:13px;height:13px}.input__container__text[type=search]{-webkit-appearance:textfield;-webkit-box-sizing:content-box}::-webkit-search-decoration{display:none}.input__container__text[type=reset],.input__container__text[type=button],.input__container__text[type=submit]{overflow:visible}.input__container__text::-webkit-scrollbar{width:16px;background-color:var(--color-shadow-0, rgba(0, 0, 0, 0.04));border-radius:10px}.input__container__text::-webkit-scrollbar-thumb{border-radius:10px;border:4px solid transparent;border-radius:10px;background-clip:content-box;background-color:var(--color-border-1, rgba(0, 0, 0, 0.2))}.input__message{display:-ms-flexbox;display:flex;-ms-flex-align:baseline;align-items:baseline;height:20px;margin:3.7px 2.5px;-webkit-transition:0.3s cubic-bezier(0.4, 0, 0.2, 1);transition:0.3s cubic-bezier(0.4, 0, 0.2, 1);color:var(--color-content-disable, #595959);word-break:break-word;height:auto;min-height:20px}.input__message bds-typo{margin-top:0px;-ms-flex-item-align:self-start;align-self:self-start}.input__message__icon{display:-ms-flexbox;display:flex;padding-right:4px;margin-top:0px;padding-top:2px}.input__message--danger .bds-icon{color:var(--color-negative, #e60f0f)}.input__message--danger .input__message__text{color:var(--color-negative, #e60f0f)}.input__message--success .input__message__icon .bds-icon{color:var(--color-positive, #10603b)}.input__message--success .input__message__text{color:var(--color-content-default, #282828)}.input__password--icon{position:relative;color:var(--color-content-disable, #595959);display:-ms-flexbox;display:flex}.input__password--icon::before{content:\"\";position:absolute;inset:-4px;border:2px solid transparent;border-radius:4px;pointer-events:none}.input__password--icon:focus-visible{outline:none}.input__password--icon:focus-visible::before{border-color:var(--color-focus, #c226fb)}";
+
+const InputPassword = class {
+  constructor(hostRef) {
+    index.registerInstance(this, hostRef);
+    this.bdsInputPasswordChange = index.createEvent(this, "bdsInputPasswordChange", 7);
+    this.bdsInputPasswordInput = index.createEvent(this, "bdsInputPasswordInput", 7);
+    this.bdsInputPasswordBlur = index.createEvent(this, "bdsInputPasswordBlur", 7);
+    this.bdsInputPasswordFocus = index.createEvent(this, "bdsInputPasswordFocus", 7);
+    this.bdsInputPasswordSubmit = index.createEvent(this, "bdsInputPasswordSubmit", 7);
+    this.bdsKeyDownBackspace = index.createEvent(this, "bdsKeyDownBackspace", 7);
+    this.refNativeInput = (el) => {
+      this.nativeInput = el;
+    };
+    this.toggleEyePassword = () => {
+      if (!this.disabled) {
+        this.openEyes = !this.openEyes;
+      }
+    };
+    this.onClickWrapper = () => {
+      this.onFocus();
+      if (this.nativeInput) {
+        this.nativeInput.focus();
+      }
+    };
+    this.onInput = (ev) => {
+      this.validationDanger = false;
+      this.validationMesage = '';
+      const input = ev.target;
+      if (input) {
+        this.value = input.value || '';
+      }
+      this.bdsInputPasswordInput.emit(ev);
+    };
+    this.onBlur = () => {
+      if (!this.nativeInput || !this.nativeInput.value) {
+        this.validationDanger = false;
+        this.validationMesage = '';
+      }
+      this.bdsInputPasswordBlur.emit();
+      this.isPressed = false;
+    };
+    this.onFocus = () => {
+      this.validationDanger = false;
+      this.validationMesage = '';
+      this.bdsInputPasswordFocus.emit();
+      this.isPressed = true;
+    };
+    this.onSubmit = () => {
+      this.bdsInputPasswordSubmit.emit();
+    };
+    this.keyPressWrapper = (event) => {
+      switch (event.key) {
+        case 'Enter':
+          this.bdsInputPasswordSubmit.emit({ event, value: this.value });
+          break;
+        case 'Backspace':
+        case 'Delete':
+          this.bdsKeyDownBackspace.emit({ event, value: this.value });
+          break;
+      }
+    };
+    this.validationDanger = false;
+    this.isPressed = false;
+    this.validationMesage = '';
+    this.openEyes = false;
+    this.value = '';
+    this.label = '';
+    this.inputName = '';
+    this.max = undefined;
+    this.maxlength = undefined;
+    this.min = undefined;
+    this.minlength = undefined;
+    this.readonly = false;
+    this.helperMessage = '';
+    this.errorMessage = '';
+    this.successMessage = '';
+    this.danger = false;
+    this.success = false;
+    this.icon = '';
+    this.disabled = false;
+    this.autoCapitalize = 'off';
+    this.autoComplete = 'off';
+    this.placeholder = '';
+    this.dataTest = null;
+  }
+  handleKeyDown(event) {
+    if (event.key == 'Enter') {
+      this.toggleEyePassword();
+    }
+  }
+  getAutoComplete() {
+    if (!this.openEyes)
+      return 'current-password';
+    return this.autoComplete;
+  }
+  onChange() {
+    this.bdsInputPasswordChange.emit({ value: this.value == null ? this.value : this.value.toString() });
+  }
+  renderIcon() {
+    return (this.icon && (index.h("div", { class: {
+        input__icon: true,
+        'input__icon--large': !!this.label,
+      } }, index.h("bds-icon", { size: this.label ? 'medium' : 'small', name: this.icon, color: "inherit" }))));
+  }
+  renderLabel() {
+    return (this.label && (index.h("label", { class: {
+        input__container__label: true,
+        'input__container__label--pressed': this.isPressed && !this.disabled,
+      } }, index.h("bds-typo", { variant: "fs-12", bold: "bold" }, this.label))));
+  }
+  renderMessage() {
+    const icon = this.danger ? 'error' : this.success ? 'checkball' : 'info';
+    let message = this.danger ? this.errorMessage : this.success ? this.successMessage : this.helperMessage;
+    if (!message && this.validationDanger)
+      message = this.validationMesage;
+    const styles = this.danger || this.validationDanger
+      ? 'input__message input__message--danger'
+      : this.success
+        ? 'input__message input__message--success'
+        : 'input__message';
+    if (message) {
+      return (index.h("div", { class: styles, part: "input__message" }, index.h("div", { class: "input__message__icon" }, index.h("bds-icon", { size: "x-small", name: icon, theme: "outline", color: "inherit" })), index.h("bds-typo", { class: "input__message__text", variant: "fs-12" }, message)));
+    }
+    return undefined;
+  }
+  componentDidUpdate() {
+    if (this.nativeInput && this.value != this.nativeInput.value) {
+      this.nativeInput.value = this.value;
+    }
+  }
+  render() {
+    const isPressed = this.isPressed && !this.disabled;
+    const iconPassword = this.openEyes ? 'eye-open' : 'eye-closed';
+    const type = this.openEyes ? 'text' : 'password';
+    const autocomplete = this.getAutoComplete();
+    return (index.h(index.Host, { "aria-disabled": this.disabled ? 'true' : null }, index.h("div", { class: {
+        input: true,
+        'input--state-primary': !this.danger && !this.validationDanger,
+        'input--state-danger': this.danger || this.validationDanger,
+        'input--state-success': this.success,
+        'input--state-disabled': this.disabled,
+        'input--label': !!this.label,
+        'input--pressed': isPressed,
+      }, onClick: this.onClickWrapper, onKeyDown: this.keyPressWrapper, part: "input-container" }, this.renderIcon(), index.h("div", { class: "input__container" }, this.renderLabel(), index.h("div", { class: { input__container__wrapper: true } }, index.h("input", { ref: this.refNativeInput, class: { input__container__text: true }, type: type, name: this.inputName, min: this.min, max: this.max, minLength: this.minlength, maxLength: this.maxlength, readOnly: this.readonly, autocomplete: autocomplete, autocapitalize: this.autoCapitalize, placeholder: this.placeholder, onInput: this.onInput, onFocus: this.onFocus, onBlur: this.onBlur, onSubmit: this.onSubmit, value: this.value, disabled: this.disabled, "data-test": this.dataTest }))), index.h("div", { class: "input__password--icon", onClick: this.toggleEyePassword, onKeyDown: this.handleKeyDown.bind(this), tabindex: "0" }, index.h("bds-icon", { size: "small", name: iconPassword, color: "inherit" })), this.success && index.h("bds-icon", { class: "icon-success", name: "check", theme: "outline", size: "xxx-small" })), this.renderMessage()));
+  }
+  static get watchers() { return {
+    "value": ["onChange"]
+  }; }
+};
+InputPassword.style = inputPasswordCss;
+
+exports.bds_input_password = InputPassword;
---
+++ pr-preview/pr-1072/collection/components/tabs/tab-group.js
@@ -0,0 +1,298 @@
+import { h, Host } from '@stencil/core';
+export class BdsTabGroup {
+  constructor() {
+    this.tabItensElement = null;
+    this.tabItensSlideElement = null;
+    this.getEventsDisable = (ItensElement) => {
+      ItensElement.forEach((element) => {
+        element.addEventListener('tabDisabled', () => {
+          this.setInternalItens(Array.from(this.tabItensElement));
+        }, false);
+      });
+    };
+    this.checkSlideTabs = () => {
+      if (this.headerElement || this.headerSlideElement) {
+        if (this.headerSlideElement?.offsetWidth > this.headerElement?.offsetWidth) {
+          return true;
+        }
+      }
+    };
+    this.setFirstActive = () => {
+      const hasOpenDefined = Array.from(this.tabItensElement).filter((obj) => obj.open);
+      if (!hasOpenDefined.length) {
+        this.tabItensElement[0].open = true;
+      }
+    };
+    this.setnumberElement = () => {
+      for (let i = 0; i < this.tabItensElement.length; i++) {
+        this.tabItensElement[i].numberElement = i;
+      }
+    };
+    this.setInternalItens = (ItensElement) => {
+      const arrayItens = ItensElement.map((item, index) => {
+        return {
+          label: item.label,
+          open: item.open,
+          numberElement: index,
+          badge: item.badge,
+          ...(item.disable !== undefined && { disable: item.disable }),
+          ...(item.icon !== undefined && { icon: item.icon }),
+          ...(item.iconPosition !== undefined && { iconPosition: item.iconPosition }),
+          ...(item.iconTheme !== undefined && { iconTheme: item.iconTheme }),
+          ...(item.badgeShape !== undefined && { badgeShape: item.badgeShape }),
+          ...(item.badgeColor !== undefined && { badgeColor: item.badgeColor }),
+          ...(item.badgeIcon !== undefined && { badgeIcon: item.badgeIcon }),
+          ...(item.badgeAnimation !== undefined && { badgeAnimation: item.badgeAnimation }),
+          ...(item.badgeNumber !== undefined && { badgeNumber: item.badgeNumber }),
+          ...(item.badgePosition !== undefined && { badgePosition: item.badgePosition }),
+          ...(item.dataTest !== undefined && { dataTest: item.dataTest }),
+        };
+      });
+      return (this.internalItens = arrayItens);
+    };
+    this.handleClick = (numberItem) => {
+      const updateInternalItens = this.internalItens.map((item) => {
+        return {
+          label: item.label,
+          open: false,
+          numberElement: item.numberElement,
+        };
+      });
+      this.internalItens = updateInternalItens;
+      for (let i = 0; i < this.tabItensElement.length; i++) {
+        if (this.tabItensElement[i].numberElement != numberItem) {
+          this.tabItensElement[i].open = false;
+        }
+        else {
+          this.tabItensElement[i].open = true;
+          this.bdsTabChange.emit(this.tabItensElement[i]);
+        }
+      }
+    };
+    this.refHeaderElement = (el) => {
+      this.headerElement = el;
+    };
+    this.refHeaderSlideElement = (el) => {
+      this.headerSlideElement = el;
+    };
+    this.handleDisabled = (id) => {
+      this.bdsTabDisabled.emit(this.tabItensElement[id]);
+    };
+    this.nextSlide = () => {
+      const minLeft = this.headerElement?.offsetWidth - this.headerSlideElement?.offsetWidth;
+      const calcNumber = this.headerSlideElement?.offsetWidth / this.headerElement?.offsetWidth;
+      const numberClicks = parseInt(calcNumber.toString());
+      const newPosition = this.positionLeft - this.headerElement?.offsetWidth;
+      this.positionLeft = newPosition < minLeft ? minLeft : newPosition;
+      this.alignTab = newPosition < minLeft ? 'right' : 'scrolling';
+      this.tabRefSlide = numberClicks <= this.tabRefSlide ? this.tabRefSlide + 1 : numberClicks;
+    };
+    this.prevSlide = () => {
+      const calcNumber = this.headerSlideElement?.offsetWidth / this.headerElement?.offsetWidth;
+      const numberClicks = parseInt(calcNumber.toString());
+      const newPosition = this.positionLeft + this.headerElement?.offsetWidth;
+      this.positionLeft = newPosition > 0 ? 0 : newPosition;
+      this.alignTab = newPosition > 0 ? 'left' : 'scrolling';
+      this.tabRefSlide = numberClicks <= this.tabRefSlide ? this.tabRefSlide - 1 : numberClicks;
+    };
+    this.renderIcon = (Icon, Theme, disable) => {
+      return (h("bds-icon", { class: { tab_group__header__itens__item__typo__disable: disable }, size: "x-small", name: Icon, theme: Theme }));
+    };
+    this.renderBadge = (Shape, Color, Icon, Animation, Number) => {
+      return (h("bds-grid", { "justify-content": "center" }, h("bds-badge", { color: Color, icon: Icon, number: Number, shape: Shape, animation: Animation })));
+    };
+    this.internalItens = undefined;
+    this.isSlideTabs = false;
+    this.alignTab = 'left';
+    this.tabRefSlide = 0;
+    this.positionLeft = 0;
+    this.contentScrollable = true;
+    this.align = 'center';
+    this.dtButtonPrev = null;
+    this.dtButtonNext = null;
+  }
+  componentWillRender() {
+    this.tabItensElement = this.element.getElementsByTagName('bds-tab-item');
+    this.setnumberElement();
+    this.setFirstActive();
+    this.setInternalItens(Array.from(this.tabItensElement));
+    this.getEventsDisable(Array.from(this.tabItensElement));
+  }
+  componentDidLoad() {
+    this.tabItensSlideElement = this.element.shadowRoot.querySelectorAll('.tab_group__header__itens__item');
+  }
+  connectedCallback() {
+    this.isSlide = window.setInterval(() => {
+      this.isSlideTabs = this.checkSlideTabs();
+    }, 100);
+  }
+  disconnectedCallback() {
+    window.clearInterval(this.isSlide);
+  }
+  handleKeyDown(event, item) {
+    if (event.key == 'Enter') {
+      item.disable ? this.handleDisabled(item.numberElement) : this.handleClick(item.numberElement);
+    }
+    if (event.key == 'ArrowRight') {
+      this.tabItensSlideElement[item.numberElement + 1].focus();
+    }
+    if (event.key == 'ArrowLeft') {
+      this.tabItensSlideElement[item.numberElement - 1].focus();
+    }
+  }
+  render() {
+    const slidePosition = { left: `${this.positionLeft}px` };
+    return (h(Host, null, h("div", { class: { tab_group: true } }, this.isSlideTabs && this.alignTab != 'left' && (h("bds-button-icon", { class: "tab_group__slide-button", icon: "arrow-left", size: "short", id: "bds-tabs-button-left", onClick: () => this.prevSlide(), dataTest: this.dtButtonPrev, variant: "secondary" })), h("div", { class: { tab_group__header: true, tab_group__slide: this.isSlideTabs }, ref: this.refHeaderElement }, h("div", { class: {
+        tab_group__header__itens: true,
+        tab_group__slide__itens: this.isSlideTabs,
+        [`tab_group__header__itens__${this.align}`]: !this.isSlideTabs,
+      }, ref: this.refHeaderSlideElement, style: slidePosition }, this.internalItens &&
+      this.internalItens.map((item, index) => {
+        const bold = item.open == true ? 'bold' : 'regular';
+        return (h("div", { class: {
+            tab_group__header__itens__item: true,
+            tab_group__header__itens__item__open: item.open,
+            tab_group__header__itens__item__disable: item.disable,
+          }, key: index, tabindex: "0", onClick: () => item.disable ? this.handleDisabled(item.numberElement) : this.handleClick(item.numberElement), onKeyDown: (ev) => this.handleKeyDown(ev, item) }, item.iconPosition === 'left' && item.icon
+          ? this.renderIcon(item.icon, item.iconTheme, item.disable)
+          : '', item.badgePosition === 'left' && item.badge
+          ? this.renderBadge(item.badgeShape, item.badgeColor, item.badgeIcon, item.badgeAnimation, item.badgeNumber)
+          : '', h("bds-typo", { class: { tab_group__header__itens__item__typo__disable: item.disable }, variant: "fs-16", bold: bold }, item.label), item.iconPosition === 'right' && item.icon
+          ? this.renderIcon(item.icon, item.iconTheme, item.disable)
+          : '', item.badgePosition === 'right' && item.badge
+          ? this.renderBadge(item.badgeShape, item.badgeColor, item.badgeIcon, item.badgeAnimation, item.badgeNumber)
+          : ''));
+      }))), this.isSlideTabs && this.alignTab != 'right' && (h("bds-button-icon", { class: "tab_group__slide-button", icon: "arrow-right", size: "short", id: "bds-tabs-button-right", onClick: () => this.nextSlide(), dataTest: this.dtButtonNext, variant: "secondary" })), h("div", { class: { tab_group__content: true, tab_group__scrolled: this.contentScrollable } }, h("slot", null)))));
+  }
+  static get is() { return "bds-tab-group"; }
+  static get encapsulation() { return "shadow"; }
+  static get originalStyleUrls() {
+    return {
+      "$": ["tab-group.scss"]
+    };
+  }
+  static get styleUrls() {
+    return {
+      "$": ["tab-group.css"]
+    };
+  }
+  static get properties() {
+    return {
+      "contentScrollable": {
+        "type": "boolean",
+        "mutable": false,
+        "complexType": {
+          "original": "boolean",
+          "resolved": "boolean",
+          "references": {}
+        },
+        "required": false,
+        "optional": true,
+        "docs": {
+          "tags": [],
+          "text": ""
+        },
+        "attribute": "content-scrollable",
+        "reflect": false,
+        "defaultValue": "true"
+      },
+      "align": {
+        "type": "string",
+        "mutable": false,
+        "complexType": {
+          "original": "'left' | 'center' | 'right'",
+          "resolved": "\"center\" | \"left\" | \"right\"",
+          "references": {}
+        },
+        "required": false,
+        "optional": false,
+        "docs": {
+          "tags": [],
+          "text": ""
+        },
+        "attribute": "align",
+        "reflect": false,
+        "defaultValue": "'center'"
+      },
+      "dtButtonPrev": {
+        "type": "string",
+        "mutable": false,
+        "complexType": {
+          "original": "string",
+          "resolved": "string",
+          "references": {}
+        },
+        "required": false,
+        "optional": true,
+        "docs": {
+          "tags": [],
+          "text": "Data test is the prop to specifically test the component action object.\ndtButtonPrev is the data-test to button prev."
+        },
+        "attribute": "dt-button-prev",
+        "reflect": false,
+        "defaultValue": "null"
+      },
+      "dtButtonNext": {
+        "type": "string",
+        "mutable": false,
+        "complexType": {
+          "original": "string",
+          "resolved": "string",
+          "references": {}
+        },
+        "required": false,
+        "optional": true,
+        "docs": {
+          "tags": [],
+          "text": "Data test is the prop to specifically test the component action object.\ndtButtonNext is the data-test to button next."
+        },
+        "attribute": "dt-button-next",
+        "reflect": false,
+        "defaultValue": "null"
+      }
+    };
+  }
+  static get states() {
+    return {
+      "internalItens": {},
+      "isSlideTabs": {},
+      "alignTab": {},
+      "tabRefSlide": {},
+      "positionLeft": {}
+    };
+  }
+  static get events() {
+    return [{
+        "method": "bdsTabChange",
+        "name": "bdsTabChange",
+        "bubbles": true,
+        "cancelable": true,
+        "composed": true,
+        "docs": {
+          "tags": [],
+          "text": "bdsTabChange. Event to return value when Tabs is change."
+        },
+        "complexType": {
+          "original": "any",
+          "resolved": "any",
+          "references": {}
+        }
+      }, {
+        "method": "bdsTabDisabled",
+        "name": "bdsTabDisabled",
+        "bubbles": true,
+        "cancelable": true,
+        "composed": true,
+        "docs": {
+          "tags": [],
+          "text": "bdsTabDisabled. Event to return value when Tabs disable is change."
+        },
+        "complexType": {
+          "original": "any",
+          "resolved": "any",
+          "references": {}
+        }
+      }];
+  }
+  static get elementRef() { return "element"; }
+}
