e3cb7b3ca5dd8244dcbddb73eae10985064020c2
samirsaliba <samirsaliba@users.noreply.github.com>
2025-10-21 21:15:56 +0000

    Deploy preview for PR 1101 🛫

---
+++ pr-preview/pr-1101/collection/utils/languages/es_ES.js
@@ -0,0 +1,43 @@
+export const esTerms = [
+  {
+    conclude: 'Finalizar',
+    from: 'Desde',
+    reset: 'Reiniciar',
+    setTheDate: 'Establecer la fecha',
+    to: 'Hasta',
+  },
+];
+export const esMonths = [
+  {
+    january: 'Enero',
+    february: 'Febrero',
+    march: 'Marzo',
+    april: 'Abril',
+    may: 'Mayo',
+    june: 'Junio',
+    july: 'Julio',
+    august: 'Agosto',
+    september: 'Septiembre',
+    october: 'Octubre',
+    november: 'Noviembre',
+    december: 'Diciembre',
+  },
+];
+export const esDays = [
+  {
+    sunday: 'Domingo',
+    monday: 'Lunes',
+    tuesday: 'Martes',
+    wednesday: 'Miércoles',
+    thursday: 'Jueves',
+    friday: 'Viernes',
+    saturday: 'Sábado',
+  },
+];
+export const esMessages = [
+  {
+    dateFormatIsIncorrect: 'El formato de fecha es incorrecto',
+    betweenPeriodOf: 'Selecciona una fecha entre el período de',
+    endDateIsEmpty: 'Selecciona la fecha de finalización',
+  },
+];
