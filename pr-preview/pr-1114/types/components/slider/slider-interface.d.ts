d2d39e1b2ddbfceb9b65bb4a581beea5dafb7559
andrew-costa-blip <andrew-costa-blip@users.noreply.github.com>
2025-11-28 16:01:55 +0000

    Remove preview for PR 1114 🛬

--- pr-preview/pr-1114/types/components/slider/slider-interface.d.ts
+++
@@ -1,6 +0,0 @@
-export type typeRange = 'fill' | 'no-linear';
-export type typeMarkers = 'default' | 'markers' | 'without-subtitle';
-export interface StepOption {
-  value: number;
-  name: string;
-}
