145d018e7704e960c0e95d04b24c9e37aad4a3a0
gadsousa <gadsousa@users.noreply.github.com>
2026-09-17 23:30:09 +0000

    Remove preview for PR 1179 🛬

--- pr-preview/pr-1179/collection/components/counter-text/counter-text-interface.js
+++
@@ -1,6 +0,0 @@
-export var CounterTextState;
-(function (CounterTextState) {
-  CounterTextState["Default"] = "default";
-  CounterTextState["Warning"] = "warning";
-  CounterTextState["Delete"] = "delete";
-})(CounterTextState || (CounterTextState = {}));
--- pr-preview/pr-1179/collection/components/upload/languages/es_ES.js
+++
@@ -1,8 +0,0 @@
-export const esTerms = [
-  {
-    uploaded: 'Archivos subidos',
-    dropHere: 'Soltar aquí para adjuntar archivo',
-    dropOrClick: 'Arrastre y suelte sus archivos aquí o haga clic para cargar el archivo',
-    formatError: 'Se produjo un error al adjuntar el archivo, inténtelo nuevamente o seleccione otro archivo',
-  },
-];
